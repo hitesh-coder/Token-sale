6b5a4a1b5e85e65ff0d3ad0333e05007315c77a4
Hiteshedi <hitesh.kotecha@edilitics.com>
2022-01-21 20:56:01 +0530

    Transfer Token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,10 +1,31 @@
 pragma solidity ^0.5.0;
 
 contract DappToken {
+    string public name = "DappToken";
+    string public symbol = "DAPP";
+    string public standard = "Dapp Token v1.0";
+    uint256 public totalSupply;
 
-    uint public totalSupply;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
 
-    constructor() public {
-        totalSupply = 1000000;
+    mapping(address => uint) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -2,9 +2,56 @@ const DappToken = artifacts.require("DappToken.sol");
 
 contract('DappToken', (accounts) => {
 
+    it("Checks if all values are correctly assign", async () => {
+        let tokenInstance = await DappToken.deployed();
+        let name = await tokenInstance.name();
+        assert.equal(name, "DappToken", "Assign name correctly");
+
+        let symbol = await tokenInstance.symbol();
+        assert.equal(symbol, "DAPP", "Assign symbol correctly");
+
+        let standard = await tokenInstance.standard();
+        assert.equal(standard, "Dapp Token v1.0", "Assign standard correctly");
+    })
+
     it("Sets total supply on deployment", async () => {
         let tokenInstance = await DappToken.deployed();
         let totalSupply = await tokenInstance.totalSupply()
         assert.equal(totalSupply.toNumber(), 1000000, "Set total supply to 10,00,000")
+
+        let adminBalance = await tokenInstance.balanceOf(accounts[0]);
+        // console.log("accounts", accounts[0])
+        assert.equal(adminBalance, 1000000, "allocates initial supply to admin")
+    })
+
+    it("transfers token ownership", async () => {
+        let tokenInstance = await DappToken.deployed();
+        tokenInstance.transfer.call(accounts[ 1 ], 9000000).then(assert.fail).catch((err) => {
+            assert(err.message.indexOf('revert') >= 0, "error message must contain revert");
+        });
+
+        let result = await tokenInstance.transfer.call(accounts[ 1 ], 900);
+        assert.equal(result, true, "Function completed successfully");
+
+        // let balance = await tokenInstance.balanceOf(accounts[ 1 ]);
+        // console.log("before",balance.toNumber());
+
+        let transaction = await tokenInstance.transfer(accounts[ 1 ], 250000, {from: accounts[0]});
+        
+        // console.log("trns", transaction)
+        assert.equal(transaction.logs.length, 1, "triggers one event");
+        assert.equal(transaction.logs[0].event, "Transfer", "Event name should be 'Transfer' ");
+        assert.equal(transaction.logs[0].args._from, accounts[0], "logs the account the token are transfered from");
+        assert.equal(transaction.logs[0].args._to, accounts[1], "logs the account the token are transfered to");
+        assert.equal(transaction.logs[0].args._value, 250000, "logs the transfered amount");
+
+
+
+        let balance = await tokenInstance.balanceOf(accounts[ 1 ]);
+        // console.log("after",balance.toNumber())
+        assert.equal(balance.toNumber(), 250000, "Balance should be added to account" );
+
+        balance = await tokenInstance.balanceOf(accounts[ 0 ]);
+        assert.equal(balance.toNumber(), 750000, "Balance should be deducted from account");
     })
 })
